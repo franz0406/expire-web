66c3f842fce31229fa36456941113661d82347b8
hwang <franz0406@naver.com>
2022-02-15 19:58:43 +0900

    stats_excerpt & latest_blog finish

--- js/main.js
+++ js/main.js
@@ -1,14 +1,69 @@
-const swiper = new Swiper('.main_slider_wrapper', {
-    loop: true,
-    speed: 400,
-    spaceBetween: 100,
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
+window.addEventListener('DOMContentLoaded', function(){
+
+    // MAIN SLIDER
+    const swiper = new Swiper('.main_slider_wrapper', {
+        loop: true,
+        speed: 400,
+        spaceBetween: 100,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    // NUMBER ANIMATION
+    const countNum = $('.stats_excerpt .count');
+
+    countNum.each((idx, elem)=>{
+
+        const waypoint = new Waypoint({
+        element: elem,
+        handler: function() {
+            const rateNum = $(elem).attr('data-rate');
+            $({count:0}).animate({count:rateNum},{
+                duration: 2000,
+                progress: function(){
+                    const currentNum = Math.floor(this.count);
+                    $(elem).text(currentNum)
+                }
+            })
+            this.destroy();
+        },
+        offset: '70%',
+        });
+
+    })
+    const prev = document.querySelector('.blog_prev');
+    const next = document.querySelector('.blog_next');
+
+    const blogSlider = new Swiper(".blog_list_slider", {
+        loop:true,
+        autoplay: true,
+        breakpoints: {
+            640: { // mobile
+              slidesPerView: 1, // 슬라이드 개수
+              spaceBetween: 30, // 슬라이드 간격
+            },
+            768: { // tablet
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            1140: { // pc
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+        },
+    })
+
+    prev.addEventListener('click',()=>{
+        blogSlider.slidePrev();
+    });
+    next.addEventListener('click',()=>{
+        blogSlider.slideNext();
+    }); 
+})
